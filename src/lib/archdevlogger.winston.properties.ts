74875863ecf3553568c25e94a6e0c53bf1d04289
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-03-07 14:01:16 +0700

    UPDATE: ArchDevLoggerWinstonProperties

--- src/lib/archdevlogger.winston.properties.ts
+++ src/lib/archdevlogger.winston.properties.ts
@@ -2,7 +2,6 @@ import { ILoggerProperties } from "archdevlogger";
 import { ICreateOptions, ILogFormatter, ILogTransporter } from "./core";
 
 export interface ArchDevLoggerWinstonProperties extends ILoggerProperties {
-  level?: string;
   formatters?: ILogFormatter[];
   transporters?: ILogTransporter[];
   createOptions?: ICreateOptions;
