787eba5bad4ad102c9ae33bd150b050355f89699
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-03-11 09:53:15 +0700

    UPDATE: IFormatter

--- src/lib/core/iformatter.ts
+++ src/lib/core/iformatter.ts
@@ -1,6 +1,6 @@
 import { ArchDevLoggerWinston } from "../archdevlogger.winston";
 
-export interface IFormatter<OptsType = any> {
-  format(logObj: any, opts: OptsType): void;
+export interface IFormatter<OptsType = any, LogObj = any> {
+  format(logObj: LogObj, opts: OptsType): LogObj;
   opts(logger: ArchDevLoggerWinston): void;
 }
